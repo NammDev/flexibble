f0982313284ecfcfec03b9472ab1915669ce0b83
Nam <namdeveloper.ca@gmail.com>
2023-09-10 13:11:29 +0700

    Add common types & constants for project, and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
+        Navbar
         <main>{children}</main>
+        Footer
       </body>
     </html>
   )
