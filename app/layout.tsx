0dbcd84637a71af2f5bce86f12e84b7ab0bf94f3
Nam <namdeveloper.ca@gmail.com>
2023-09-10 15:34:31 +0700

    Layout: Navbar & Footer styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'Flexibble',
@@ -10,9 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   )
---
+++ components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { NavLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  const session = true
+  return (
+    <nav className='flexBetween navbar'>
+      {/* Logo & Links */}
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image src='/logo.svg' width={115} height={43} alt='Flexibble' />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      {/* User Photo & Signin */}
+      <div className='flexCenter gap-4'>
+        {session ? (
+          <>
+            User Photo
+            <Link href='/create-project'>Share Work</Link>
+          </>
+        ) : (
+          <>Login </>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
