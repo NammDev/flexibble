14d73a7082e69efbf4c69727c51a6ab9641ce7bd
Nam <namdeveloper.ca@gmail.com>
2023-09-15 22:48:44 +0700

    Submit button & hanlde Submit

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -2,10 +2,11 @@
 
 import { FormState, ProjectInterface, SessionInterface } from '@/common.types'
 import Image from 'next/image'
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import FormField from './FormField'
 import CustomMenu from './CustomMenu'
 import { categoryFilters } from '@/constants'
+import Button from './Button'
 
 type Props = {
   type: string
@@ -14,13 +15,7 @@ type Props = {
 }
 
 const ProjectForm = ({ type, session, project }: Props) => {
-  const handleFormSubmit = (e: React.FormEvent) => {}
-
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {}
-
-  const handleStateChange = (fieldName: keyof FormState, value: string) => {
-    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }))
-  }
+  const [submitting, setSubmitting] = useState<boolean>(false)
 
   const [form, setForm] = useState<FormState>({
     title: project?.title || '',
@@ -30,6 +25,38 @@ const ProjectForm = ({ type, session, project }: Props) => {
     githubUrl: project?.githubUrl || '',
     category: project?.category || '',
   })
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    // prevent browser reload page
+    e.preventDefault()
+    const file = e.target.files?.[0]
+    if (!file) return
+    if (!file.type.includes('image')) {
+      return alert('Please upload an image file')
+    }
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      const result = reader.result as string
+      handleStateChange('image', result)
+    }
+  }
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }))
+  }
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    setSubmitting(true)
+    try {
+      console.log(form)
+    } catch (error) {
+      alert(`Failed to ${type === 'create' ? 'create' : 'edit'} a project. Try again!`)
+    } finally {
+      setSubmitting(false)
+    }
+  }
 
   return (
     <form onSubmit={handleFormSubmit} className='flexStart form'>
@@ -92,6 +119,19 @@ const ProjectForm = ({ type, session, project }: Props) => {
         filters={categoryFilters}
         setState={(value) => handleStateChange('category', value)}
       />
+
+      <div className='flexStart w-full'>
+        <Button
+          title={
+            submitting
+              ? `${type === 'create' ? 'Creating' : 'Editing'}`
+              : `${type === 'create' ? 'Create' : 'Edit'}`
+          }
+          type='submit'
+          leftIcon={submitting ? '' : '/plus.svg'}
+          submitting={submitting}
+        />
+      </div>
     </form>
   )
 }
